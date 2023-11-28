dec5d272c0950f80691db9b41efc1f6419266633
Jack Tarricone <jack.tarricone@nasa.gov>
2023-11-28 10:40:34 -0500

    yaht

--- spicy_test.R
+++ spicy_test.R
@@ -2,18 +2,27 @@ library(terra)
 library(ncdf4)
 
 
-banner <-rast("~/spicy/SnowEx-Data/Banner_2021-03-15.nc")
+
 
 # path to file
-nc_path <-x
-nc_in <-nc_open("~/spicy/SnowEx-Data/Banner_2021-03-15.nc") # open
+nc_in <-nc_open("~/spicy/rasters/SnowEx-Data/Little_Cottonwood_2021-03-18.nc") # open
 nc_in
 
+
 # pull out needed info
-projection <-ncvar_get(nc_in,"crs_wkt")
-sd_mat <-rast(ncvar_get(nc_in,"snow_depth")) # read in
-sd_mat 
-sd_proj <- wkt("GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Latitude",NORTH],AXIS["Longitude",EAST],AUTHORITY["EPSG","4326"]]"
+sd_rast <-rast(ncvar_get(nc_in,"snow_depth")) # read in
+extent <-ext(-111.720928684407, -111.614772255332, 40.5202025157807, 40.5984294421262)
+ext(sd_rast) <-extent
+crs(sd_rast) <-'EPSG:4326'
+plot(sd_rast[[1]])
+
+# pull out needed info
+rast <-rast(ncvar_get(nc_in,"lidar-sd")) # read in
+extent <-ext(-111.720928684407, -111.614772255332, 40.5202025157807, 40.5984294421262)
+ext(rast) <-extent
+crs(rast) <-'EPSG:4326'
+plot(rast[[1]])
+
 
 # make test rast from one day
 flip_mat <-apply(t(sc_mat),2,rev) # rotate matrix 90 deg counter clockwise
